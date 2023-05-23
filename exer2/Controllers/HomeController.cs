a9d1161ec896d069d4b3b744e5156da478120e29
ahmedbarami <baramiahmed9@gmail.com>
2023-05-23 09:54:36 +0300

    projct calculator

---
+++ exer2/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using exer2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace exer2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult calc()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult calc(Numbers numbers)
+        {
+            int result = 0;
+
+            switch (numbers.Opertion)
+            {
+                case '+': result = numbers.nOne + numbers.nTwo;break;
+                case '-': result = numbers.nOne - numbers.nTwo;break;
+
+                case '*': result = numbers.nOne * numbers.nTwo; break;
+                case '/': if (numbers.nTwo == 0)
+                            {
+                                result = -1; 
+                            }
+                            else
+                            {
+                                result = numbers.nOne + numbers.nTwo;
+                            }
+                            break;
+
+                default:
+                    break;
+            }
+            ViewBag.n = result;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ exer2/Models/Numbers.cs
@@ -0,0 +1,9 @@
+﻿namespace exer2.Models
+{
+    public class Numbers
+    {
+        public int nOne { get; set; }
+        public int nTwo { get; set; }
+        public char Opertion { get; set; }
+    }
+}
